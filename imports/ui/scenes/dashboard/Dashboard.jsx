25673b28404b8cd93a3df0b5759e9e70f0aea431
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-02-17 12:00:58 -0800

    Basic dashboard profile settings layout 2

--- imports/ui/scenes/dashboard/Dashboard.jsx
+++ imports/ui/scenes/dashboard/Dashboard.jsx
@@ -12,10 +12,6 @@ import { getEmailAddressesFromContacts } from '/imports/api/util';
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-
-    // this.state = {
-    //   isAvatarUploading: false,
-    // };
   }
 
   render() {
@@ -34,108 +30,6 @@ class Dashboard extends Component {
       </div>
     );
   }
-
-  //
-  // handleLogout = e => {
-  //   Meteor.logout((err) => {
-  //     if (err) {
-  //       alert(err);
-  //       return;
-  //     }
-  //
-  //     this.props.history.push('/');
-  //   })
-  // };
-  //
-  // handleAddContact = e => {
-  //   e.preventDefault();
-  //
-  //   Meteor.call('email.add', this.newEmailElement.value, (err) => {
-  //     if (err) {
-  //       alert(err);
-  //       return;
-  //     }
-  //     alert('New email added');
-  //   })
-  // };
-  //
-  // handleUploadAvatar = e => {
-  //   e.preventDefault();
-  //
-  //   const file = this.avatarInput.files[0];
-  //   if (file) {
-  //     const uploadInstance = Images.insert({
-  //       file: file,
-  //       streams: 'dynamic',
-  //       chunkSize: 'dynamic'
-  //     }, false);
-  //
-  //     uploadInstance.on('start', () => {
-  //       this.setState({
-  //         isAvatarUploading: true,
-  //       })
-  //     });
-  //
-  //     uploadInstance.on('end', (error, fileObj) => {
-  //       if (error) {
-  //         window.alert('Error during upload: ' + error.reason);
-  //       } else {
-  //         window.alert('File "' + fileObj.name + '" successfully uploaded');
-  //       }
-  //       Meteor.call('image.setAvatar', fileObj, (err) => {
-  //         if (err) {
-  //           window.alert('Error during updating avatar: ' + error.reason);
-  //         } else {
-  //           window.alert('Avatar updated successful');
-  //         }
-  //
-  //         this.setState({
-  //           isAvatarUploading: false,
-  //         });
-  //       });
-  //     });
-  //
-  //     uploadInstance.start();
-  //   }
-  // };
-  //
-  // render() {
-  //   const {
-  //     currentUser
-  //   } = this.props;
-  //
-  //   return (
-  //     <div>
-  //       Dashboard works!
-  //       <button onClick={this.handleLogout}>Logout</button>
-  //
-  //
-  //       <div>
-  //         <h3>Add email</h3>
-  //         <form onSubmit={this.handleAddContact}>
-  //           <input type="text" name="email" ref={el => this.newEmailElement = el} />
-  //           <button type="submit">Submit</button>
-  //         </form>
-  //
-  //         {getEmailAddressesFromContacts(currentUser.profile.contacts).map((e) => {
-  //             return (
-  //               <div>{e}</div>
-  //             )
-  //           })
-  //         }
-  //       </div>
-  //
-  //       <div>
-  //         <h3>Update Image</h3>
-  //         <form onSubmit={this.handleUploadAvatar}>
-  //           <input disabled={this.state.isAvatarUploading} id="fileInput" type="file" ref={el => this.avatarInput = el} />
-  //           <p><small>Upload file in <code>jpeg</code> or <code>png</code> format, with size less or equal to 10MB</small></p>
-  //           <button type="submit">Submit</button>
-  //         </form>
-  //       </div>
-  //     </div>
-  //   )
-  // }
 }
 
 export default withTracker((props) => {
